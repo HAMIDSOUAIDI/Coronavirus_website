4fa3ca2ffe3a204a5c23a8f9632198b46d809230
HAMIDSOUAIDI <66180251+HAMIDSOUAIDI@users.noreply.github.com>
2020-08-05 15:35:40 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+$(document).ready(function () {
+    $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function () {
+        $(this).toggleClass('open');
+    });
+});
+function myFunction() {
+    var x = document.getElementById("mea");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
 am4core.ready(function () {
 
     // Themes begin
